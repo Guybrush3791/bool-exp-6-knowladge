45e397011e2c2f7b0e5b97c876d07e720d4502db
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-12 11:56:44 +0200

    vault backup: 2023-09-12 11:56:44

--- Workspace/JavaTest_2/src/org/experis/Main.java
+++ Workspace/JavaTest_2/src/org/experis/Main.java
@@ -2,4 +2,12 @@ package org.experis;
 
 public class Main {
 
+	public static void main(String[] args) {
+		
+		System.out.println("Ciao, Mondo!");
+		
+		if (10 < 5); {
+			System.out.println("10 minore di 5");
+		}
+	}
 }
