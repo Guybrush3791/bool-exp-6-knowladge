8e91dc599e0453087da8e631f098bbf04a78dcae
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-12 11:46:44 +0200

    vault backup: 2023-09-12 11:46:44

--- Workspace/JavaTest_1/src/org/experis/Main.java
+++ Workspace/JavaTest_1/src/org/experis/Main.java
@@ -1,8 +1,12 @@
 package org.experis;
 
+import java.lang.Math;
+
 public class Main {
 
 	public static void main(String[] args) {
+		
+		double x = Math.pow(2, 2);
 		
 		System.out.println("Hello, World!");
 		System.out.println("Hola, Mundo!");
