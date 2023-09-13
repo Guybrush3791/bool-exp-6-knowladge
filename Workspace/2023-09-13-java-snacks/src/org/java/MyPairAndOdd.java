e625dc2fe3cdccafd0b1309aca2ddeab36ecbee1
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-13 15:19:44 +0200

    vault backup: 2023-09-13 15:19:44

--- Workspace/2023-09-13-java-snacks/src/org/java/MyPairAndOdd.java
+++ Workspace/2023-09-13-java-snacks/src/org/java/MyPairAndOdd.java
@@ -20,10 +20,22 @@ public class MyPairAndOdd {
 		System.out.print("Dammi il tuo numero: ");
 		int userValue = sc.nextInt();
 		
-		Random rnd = new Random();
-		
-		int pcValue = rnd.nextInt(Integer.MAX_VALUE);
-		
 		sc.close();
+		
+		Random rnd = new Random();
+		int pcValue = rnd.nextInt(100);
+		System.out.println("pc value: " + pcValue);
+		
+		boolean isSumPair = ((userValue + pcValue) % 2) == 0;
+
+		System.out.println("\n----------------------------------------\n");
+		
+		if (pair == isSumPair) {
+			
+			System.out.println("L'utente ha vinto!! Yheeee!!!");
+		} else {
+			
+			System.out.println("You loose!!");
+		}
 	}
 }
