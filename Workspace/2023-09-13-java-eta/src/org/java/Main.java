f22b7f08a6b36e417bf9fe2ffe2c4e4699b1cd5a
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-13 14:39:44 +0200

    vault backup: 2023-09-13 14:39:44

--- Workspace/2023-09-13-java-eta/src/org/java/Main.java
+++ Workspace/2023-09-13-java-eta/src/org/java/Main.java
@@ -1,9 +1,30 @@
 package org.java;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Eta' persona 1: ");
+		int eta1 = sc.nextInt();
+		
+		System.out.print("Eta' persona 2: ");
+		int eta2 = sc.nextInt();
+		
+		sc.close();
+		
+		if (eta1 > eta2) {
+			
+			System.out.println("Persona 1 e' piu' grande");
+		} else if (eta2 > eta1) {
+			
+			System.out.println("Persona 2 e' piu' grande");
+		} else {
+			
+			System.out.println("Persona 1 e 2 hanno la stessa eta'");
+		}
 	}
 }
