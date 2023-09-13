f5582537a23063854fe0e61dd88f9bb528e90f1b
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-13 13:04:44 +0200

    vault backup: 2023-09-13 13:04:44

--- Workspace/JavaTest_2/src/org/java/Main.java
+++ Workspace/JavaTest_2/src/org/java/Main.java
@@ -1,5 +1,9 @@
 package org.java;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -24,6 +28,8 @@ public class Main {
 		// OGGETTI
 		String str = "my string";
 		Scanner sc = new Scanner(System.in);
+
+		System.out.println("// -----------------------------------------");
 		
 		// -----------------------------------------
 		
@@ -46,15 +52,130 @@ public class Main {
 		String intConcat = "" + x + y;
 		System.out.println("intConcat: " + intConcat);
 		
+		System.out.println("// -----------------------------------------");
+		
 		// -----------------------------------------
 		
 		// LETTURA/SCRITTURA DA TERMINALE
-		String line = null;
-		Scanner in = new Scanner(System.in);
+//		String line = null;
+//		Scanner in = new Scanner(System.in);
+//		
+//		System.out.print("Type something: ");
+//		line = in.nextLine();
+//		
+//		System.out.println("You typed: '" + line + "'");
+//		
+//		System.out.println("// -----------------------------------------");
 		
-		System.out.print("Type something: ");
-		line = in.nextLine();
+		// -----------------------------------------
 		
-		System.out.println("You typed: '" + line + "'");
+		// OPERATORI CONDIZIONALI
+		
+		int x1 = 10;
+		int x2 = 10;
+		int x3 = 20;
+		int x4 = 50;
+		
+		if (x1 == 10) {
+			
+			System.out.println("x1 is 10");			
+		}
+		
+		if (x1 > x2) { 
+			
+			System.out.println("x1 is gt x2");
+		} else if (x3 < x4) {
+			
+			System.out.println("x3 is lt x4");
+		} else {
+			
+			System.out.println("I don't really f***ing know");
+		}
+		
+		System.out.println("// -----------------------------------------");
+		
+		// -----------------------------------------
+		
+		// CLASSE RANDOM
+		
+		Random rnd = new Random();
+		rnd.setSeed(100);
+		
+		int val = rnd.nextInt(10, 20);
+		System.out.println("val: " + val);
+		
+		System.out.println("// -----------------------------------------");
+		
+		// -----------------------------------------
+		
+		// TYPE CASTING
+		
+		int myInt = 100;
+		long myLong = myInt;
+		System.out.println("myLong: " + myLong);
+		
+		long myLong2 = 2_147_483_650L;
+		int myInt2 = (int) myLong2;
+		System.out.println("myInt2: " + myInt2);
+		
+		System.out.println("// -----------------------------------------");
+		
+		// -----------------------------------------
+		
+		// STRING
+		
+		String myStr = "Hello, World!";
+		System.out.println("myStr: " + myStr);
+		
+		int lng = myStr.length();
+		System.out.println("lng: " + lng);
+		
+		String myStrUpper = myStr.toUpperCase();
+		System.out.println("myStrUpper: " + myStrUpper);
+		
+		float f2 = 10.23944F;
+		String formattedF2 = String.format("%.04f", f2);
+		System.out.println("formattedF2: " + formattedF2);
+		
+		int i2 = 3;
+		String formattedI2 = String.format("hello: %06d", i2);
+		System.out.println("formattedI2: " + formattedI2);
+		
+		System.out.println("// -----------------------------------------");	
+		
+		// -----------------------------------------
+		
+		// ARRAY
+		int[] arr1 = { 1, 2, 3, 4, 5 };
+		System.out.println(arr1);
+		System.out.println(Arrays.toString(arr1));
+		
+		int[] arr2 = new int[10];
+		
+		arr2[0] = 100;
+		arr2[9] = 200;
+		
+		System.out.println(arr2[0]);
+		System.out.println(arr2[9]);
+		System.out.println(arr2[1]);
+		
+		// -----------------------------------------
+		// -----------------------------------------
+		// -----------------------------------------
+		// -----------------------------------------
+		// -----------------------------------------
+		// -----------------------------------------
+		// -----------------------------------------
+		// -----------------------------------------
+		// -----------------------------------------
+		// -----------------------------------------
+		// -----------------------------------------
+		// -----------------------------------------
+		// -----------------------------------------
+		// -----------------------------------------
+		// -----------------------------------------
+		// -----------------------------------------
+		// -----------------------------------------
+		
 	}
 }
