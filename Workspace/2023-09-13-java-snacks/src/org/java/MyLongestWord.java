2bbd7214d5da22717cf5a4530802228957fd0a45
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-13 14:49:44 +0200

    vault backup: 2023-09-13 14:49:44

--- Workspace/2023-09-13-java-snacks/src/org/java/MyLongestWord.java
+++ Workspace/2023-09-13-java-snacks/src/org/java/MyLongestWord.java
@@ -1,5 +1,30 @@
 package org.java;
 
+import java.util.Scanner;
+
 public class MyLongestWord {
 
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("word 1: ");
+		String word1 = sc.nextLine();
+		
+		System.out.print("word 2: ");
+		String word2 = sc.nextLine();
+		
+		sc.close();
+		
+		if (word1.length() > word2.length()) {
+			
+			System.out.println(word1 + " > " + word2);
+		} else if (word1.length() < word2.length()) {
+			
+			System.out.println(word2 + " > " + word1);
+		} else {
+			
+			System.out.println(word2 + " == " + word1);
+		}
+	}
 }
