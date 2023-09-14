aee509bec943351e58f19702ab25091af0a2e8e2
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-14 09:31:17 +0200

    vault backup: 2023-09-14 09:31:17

--- Workspace/2023-09-13-java-password-generator/src/org/lessons/java/security/PasswordGenerator.java
+++ Workspace/2023-09-13-java-password-generator/src/org/lessons/java/security/PasswordGenerator.java
@@ -1,5 +1,41 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
 
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Nome: ");
+		String name = sc.nextLine();
+		
+		System.out.print("\nCognome: ");
+		String lastname = sc.nextLine();
+
+		System.out.print("\nColore preferito: ");
+		String color = sc.nextLine();
+		
+		System.out.print("\nGiorno di nascita: ");
+		int birthDay = sc.nextInt();
+
+		System.out.print("\nMese di nascita: ");
+		int birthMonth = sc.nextInt();
+		
+		System.out.print("\nAnno di nascita: ");
+		int birthYear = sc.nextInt();		
+		
+		sc.close();
+		
+		String password = name + "-" + lastname + "-" 
+							+ color + "-" + (birthDay + birthMonth + birthYear);
+		
+		System.out.println("Password: " + password);
+	}
 }
+
+
+
+
+
