d2109f0be9688bafe5efb0775c5fe9345bda19ff
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-14 10:16:17 +0200

    vault backup: 2023-09-14 10:16:17

--- Workspace/2023-09-14-java-test/src/org/java/Main.java
+++ Workspace/2023-09-14-java-test/src/org/java/Main.java
@@ -4,9 +4,19 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		for (int x=0;x<10;x++) {
-			
+//		for (int x=0;x<10;x++) {
+//			
+//			System.out.println(x);
+//		}
+		int x = 0;
+		for (;;) {
+//			System.out.println(x++);
 			System.out.println(x);
+			x++;
+			
+			if (x >= 10) break;
 		}
+		
+		System.out.println("the end");
 	}
 }
