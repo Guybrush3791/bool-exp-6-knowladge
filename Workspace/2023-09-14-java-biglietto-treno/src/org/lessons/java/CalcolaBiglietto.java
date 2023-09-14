27f1e9298a6796c498c27bc14692c8bf16af5d43
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-14 09:41:17 +0200

    vault backup: 2023-09-14 09:41:17

--- Workspace/2023-09-14-java-biglietto-treno/src/org/lessons/java/CalcolaBiglietto.java
+++ Workspace/2023-09-14-java-biglietto-treno/src/org/lessons/java/CalcolaBiglietto.java
@@ -1,5 +1,33 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class CalcolaBiglietto {
 
+	public static void main(String[] args) {
+		
+		float pricePerKm = .21F;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Kilometri: ");
+		int km = sc.nextInt();
+		
+		System.out.print("\nEta': ");
+		int age = sc.nextInt();
+		
+		sc.close();
+		
+		float price = km * pricePerKm; // prezzo intero senza sconti
+		
+		if (age < 18) {
+			
+			price = price / 100 * 80;
+		} else if (age > 65) {
+			
+			price = price / 100 * 60;
+		}
+		
+		System.out.println("Prezzo finale: " + price);		
+	}
 }
