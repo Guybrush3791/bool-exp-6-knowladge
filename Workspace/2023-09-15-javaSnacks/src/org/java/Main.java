0669f615ffb18163dcbd37efaac36a5086525014
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-15 11:07:43 +0200

    vault backup: 2023-09-15 11:07:43

--- Workspace/2023-09-15-javaSnacks/src/org/java/Main.java
+++ Workspace/2023-09-15-javaSnacks/src/org/java/Main.java
@@ -250,50 +250,50 @@ public class Main {
 //		System.out.println("sum: " + sum);
 		
 		// Snack 10
-		Random rnd = new Random();
-		
-		final int lng = 10;
-		int[] values = new int[lng];
-		
-		for (int x=0;x<10;x++) {
-			
-			final int value = rnd.nextInt(100, 1001);
-			values[x] = value;
-		}
-		
-		for (int x=0;x<lng;x++) {
-			
-			final int value = values[x];
-			
-			if (value % 2 == 0) { // e' pari
-				
-				System.out.println("valore pari: " + value);
-			}
-		}
-		System.out.println("\n-----------------------\n");
-		for (int x=0;x<lng;x++) {
-			
-			final int value = values[x];
-			
-			if (value % 2 > 0) { // e' pari
-				
-				System.out.println("valore dispari: " + value);
-			}
-		}
-		System.out.println("\n-----------------------\n");
-		for (int x=0;x<lng;x+=2) {
-			
-			final int value = values[x];
-			
-			System.out.println("valore indice pari: [" + x + "]: " + value);
-		}
-		System.out.println("\n-----------------------\n");
-		for (int x=1;x<lng;x+=2) {
-			
-			final int value = values[x];
-			
-			System.out.println("valore indice dispari: [" + x + "]: " + value);
-		}
+//		Random rnd = new Random();
+//		
+//		final int lng = 10;
+//		int[] values = new int[lng];
+//		
+//		for (int x=0;x<10;x++) {
+//			
+//			final int value = rnd.nextInt(100, 1001);
+//			values[x] = value;
+//		}
+//		
+//		for (int x=0;x<lng;x++) {
+//			
+//			final int value = values[x];
+//			
+//			if (value % 2 == 0) { // e' pari
+//				
+//				System.out.println("valore pari: " + value);
+//			}
+//		}
+//		System.out.println("\n-----------------------\n");
+//		for (int x=0;x<lng;x++) {
+//			
+//			final int value = values[x];
+//			
+//			if (value % 2 > 0) { // e' pari
+//				
+//				System.out.println("valore dispari: " + value);
+//			}
+//		}
+//		System.out.println("\n-----------------------\n");
+//		for (int x=0;x<lng;x+=2) {
+//			
+//			final int value = values[x];
+//			
+//			System.out.println("valore indice pari: [" + x + "]: " + value);
+//		}
+//		System.out.println("\n-----------------------\n");
+//		for (int x=1;x<lng;x+=2) {
+//			
+//			final int value = values[x];
+//			
+//			System.out.println("valore indice dispari: [" + x + "]: " + value);
+//		}
 	}
 }
 
