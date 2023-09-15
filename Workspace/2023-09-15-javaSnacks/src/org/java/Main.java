856aaed5c7ce1b2d04b07cb7f18f91bbd5cabe8e
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-15 09:32:43 +0200

    vault backup: 2023-09-15 09:32:43

--- Workspace/2023-09-15-javaSnacks/src/org/java/Main.java
+++ Workspace/2023-09-15-javaSnacks/src/org/java/Main.java
@@ -1,5 +1,6 @@
 package org.java;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -7,26 +8,61 @@ public class Main {
 	public static void main(String[] args) {
 		
 		// Snack 1
-		Scanner sc = new Scanner(System.in);
+//		Scanner sc = new Scanner(System.in);
+//		
+//		System.out.print("value: ");
+//		int value = sc.nextInt();
+//		
+//		sc.close();
+//		
+////		if (value % 2 == 0) {
+////			
+////			System.out.println("value: " + value);
+////		} else {
+////			
+////			System.out.println("value: " + (value + 1));
+////		}
+//		
+//		System.out.println("value: " + (value + (
+//				value % 2 == 0
+//				? 0
+//				: 1
+//			))
+//		);
 		
-		System.out.print("value: ");
-		int value = sc.nextInt();
+		// Snack 2
+		Random rnd = new Random();
 		
-		sc.close();
+		String[] names = { "name1", "name2", "name3" };
+		String[] lastnames = { "lastname1", "lastname2", "lastname3", "lastname4"};
 		
-//		if (value % 2 == 0) {
-//			
-//			System.out.println("value: " + value);
-//		} else {
-//			
-//			System.out.println("value: " + (value + 1));
-//		}
-		
-		System.out.println("value: " + (value + (
-				value % 2 == 0
-				? 0
-				: 1
-			))
-		);
+		for (int x=0;x<5;x++) {
+			
+			int rndNameIndex = rnd.nextInt(names.length);
+			int rndLastnameIndex = rnd.nextInt(lastnames.length);
+			
+			String name = names[rndNameIndex];
+			String lastname = lastnames[rndLastnameIndex];
+			
+			System.out.println("fullname: " + name + " " + lastname);
+		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
