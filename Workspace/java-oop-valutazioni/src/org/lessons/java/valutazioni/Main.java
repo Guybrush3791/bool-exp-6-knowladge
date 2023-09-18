073ef997019d00109534e33d78908f3216ad41b2
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-18 09:48:41 +0200

    vault backup: 2023-09-18 09:48:41

--- Workspace/java-oop-valutazioni/src/org/lessons/java/valutazioni/Main.java
+++ Workspace/java-oop-valutazioni/src/org/lessons/java/valutazioni/Main.java
@@ -1,9 +1,34 @@
 package org.lessons.java.valutazioni;
 
+import java.util.Random;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello, World!");
+		final int LNG = 20;
+		Studente[] studenti = new Studente[LNG];
+		
+		Random rnd = new Random();
+		for (int x=0;x<LNG;x++) {
+			
+			Studente s = new Studente(
+					x + 1, 
+					rnd.nextInt(0, 101),
+					rnd.nextFloat(0, 5)
+			);
+			
+			studenti[x] = s;
+			
+			System.out.println("Studente " + s.id);
+			System.out.println("----------------");
+			System.out.println("assenze: " + s.assenze + "%");
+			System.out.println("media voti: " + 
+					String.format("%.02f", s.mediaVoti)
+					+ "/5");
+			System.out.println("\nbocciato: " + (s.isBocciato() ? "YES" : "NO"));
+			
+			System.out.println("\n\n");
+		}
 	}
 }
