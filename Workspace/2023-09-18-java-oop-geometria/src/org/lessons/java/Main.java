367688d25361b7551e8e5b9ba5b059acf0454109
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-18 09:23:41 +0200

    vault backup: 2023-09-18 09:23:41

---
+++ Workspace/2023-09-18-java-oop-geometria/src/org/lessons/java/Main.java
@@ -0,0 +1,9 @@
+package org.lessons.java;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, World!");
+	}
+}
---
+++ Workspace/2023-09-18-java-oop-geometria/src/org/lessons/java/geometria/Rettangolo.java
@@ -0,0 +1,5 @@
+package org.lessons.java.geometria;
+
+public class Rettangolo {
+
+}
