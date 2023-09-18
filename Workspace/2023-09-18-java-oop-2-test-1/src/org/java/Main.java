de22c1a5745b32a468b485467907afa8156afd10
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-18 11:58:41 +0200

    vault backup: 2023-09-18 11:58:41

--- Workspace/2023-09-18-java-oop-2-test-1/src/org/java/Main.java
+++ Workspace/2023-09-18-java-oop-2-test-1/src/org/java/Main.java
@@ -2,4 +2,44 @@ package org.java;
 
 public class Main {
 
+	public static class Square {
+		
+		private int x;
+		
+		public Square(int x) {
+		
+			setX(x);
+		}
+		
+		public int getX() {
+			
+			return x;
+		}
+		public void setX(int x) {
+			
+			this.x = x;
+		}
+		
+		public int getArea() {
+			
+			return x * x;
+		}
+		public int getPerim() {
+			
+			return 4 * x;
+		}
+		
+		@Override
+		public String toString() {
+			
+			return "square: " + x + " | a: " + getArea() + " | p: " + getPerim();
+		}
+	}
+//	public static class Rectangle extends Square {}
+	
+	public static void main(String[] args) {
+		
+		Square s = new Square(5);
+		System.out.println(s);
+	}
 }
