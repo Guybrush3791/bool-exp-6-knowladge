d0631f3376e3eb5096973346d554d054ca148bff
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-18 12:28:41 +0200

    vault backup: 2023-09-18 12:28:41

--- Workspace/2023-09-18-java-oop-2-test-2/src/org/java/Main.java
+++ Workspace/2023-09-18-java-oop-2-test-2/src/org/java/Main.java
@@ -11,7 +11,9 @@ public class Main {
 		
 		public Person(String name, String lastname, String dateOfBirth) {
 			
-			
+			setName(name);
+			setLastname(lastname);
+			setDateOfBirth(dateOfBirth);
 		}
 		
 		public String getName() {
@@ -40,10 +42,66 @@ public class Main {
 			
 			this.dateOfBirth = dateOfBirth;
 		}
+		
+		public String getFullname() {
+			
+			return getName() + " " + getLastname();
+		}
+		
+		@Override
+		public String toString() {
+
+			return getFullname() + " | date of birth: " + getDateOfBirth();
+		}
+	}
+	public static class Employee extends Person {
+		
+		private String title;
+		private int salary;
+		
+		public Employee(
+				String name, String lastname, String dateOfBirth,
+				String title, int salary
+			) { 
+			
+			super(name, lastname, dateOfBirth);
+			
+			setTitle(title);
+			setSalary(salary);
+		}
+		
+		public String getTitle() {
+			
+			return title;
+		}
+		public void setTitle(String title) {
+			
+			this.title = title;
+		}
+		public int getSalary() {
+			
+			return salary;
+		}
+		public void setSalary(int salary) {
+			
+			this.salary = salary;
+		}
+		
+		public int getYearSalary() {
+			
+			return getSalary() * 14;
+		}
+		
+		@Override
+		public String toString() {
+			
+			return super.toString() + " | year salary: " + getYearSalary(); 
+		}
 	}
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Hello, World!");
+		Person p1 = new Person("Guybrush", "Threepwood", "15/10/1989");
+		System.out.println(p1);
 	}
 }
