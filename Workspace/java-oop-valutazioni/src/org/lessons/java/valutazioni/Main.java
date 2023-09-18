e2c4305db72c6e6e7da2910a906a9aacd28c3b31
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-18 10:08:41 +0200

    vault backup: 2023-09-18 10:08:41

--- Workspace/java-oop-valutazioni/src/org/lessons/java/valutazioni/Main.java
+++ Workspace/java-oop-valutazioni/src/org/lessons/java/valutazioni/Main.java
@@ -64,9 +64,27 @@ public class Main {
 				minVoto = s.mediaVoti;
 			}
 		}
+		
+		Studente promossoMaxVoto = null;
+		float maxVoto = Float.MIN_VALUE;
+		for (int x=0;x<promossoIndex;x++) {
+			
+			Studente s = promossi[x];
+			
+			if (maxVoto < s.mediaVoti) {
+				
+				promossoMaxVoto = promossi[x];
+				maxVoto = s.mediaVoti;
+			}
+		}
+		
 		System.out.println("Voto minimo per bocciati: " 
 				+ String.format("%.02f", minVoto) + "/5"
 				+ " (id: " + bocciatoMinVoto.id + ")"
+		);
+		System.out.println("Voto massimo per promossi: " 
+				+ String.format("%.02f", maxVoto) + "/5"
+				+ " (id: " + promossoMaxVoto.id + ")"
 		);
 	}
 }
