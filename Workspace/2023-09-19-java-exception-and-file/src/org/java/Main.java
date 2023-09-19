c546c6e66403201cf2653b1ae7c603e9f5f4c0e4
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-19 12:31:09 +0200

    vault backup: 2023-09-19 12:31:09

--- Workspace/2023-09-19-java-exception-and-file/src/org/java/Main.java
+++ Workspace/2023-09-19-java-exception-and-file/src/org/java/Main.java
@@ -8,6 +8,29 @@ public class Main {
 		
 		Scanner sc = new Scanner(System.in);
 		
+		System.out.print("Numero persone: ");
+		int persCount = Integer.valueOf(sc.nextLine());
+		
+		for (int x=0;x<persCount;x++) {
+			
+			try {
+		
+				System.out.print("Name: ");
+				String name = sc.nextLine();
+				
+				System.out.print("Lastname: ");
+				String lastname = sc.nextLine();
+				
+				System.out.print("Age: ");
+				int age = Integer.valueOf(sc.nextLine());
+				
+				Persona p = new Persona(name, lastname, age);
+			} catch (Exception e) {
+				
+				x--;
+			}
+		}
+		
 		sc.close();
 	}
 }
