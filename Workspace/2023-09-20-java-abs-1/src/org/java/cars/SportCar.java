2e4cfffc90cc99705836f2a7a80bc543fdf2c74a
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-20 11:57:32 +0200

    vault backup: 2023-09-20 11:57:32

--- Workspace/2023-09-20-java-abs-1/src/org/java/cars/SportCar.java
+++ Workspace/2023-09-20-java-abs-1/src/org/java/cars/SportCar.java
@@ -1,5 +1,30 @@
 package org.java.cars;
 
-public class SportCar {
+import org.java.cars.abs.Auto;
 
+public class SportCar extends Auto {
+
+	private static final int DOORS_COUNT = 2;
+	
+	public SportCar(String name, int maxSpeed) {
+		super(name, maxSpeed);
+	}
+
+	@Override
+	public void stampaNumeroPorte() {
+		
+		System.out.println("Doors count: " + DOORS_COUNT);
+	}
+
+	public void attivaIlTurbo() {
+		
+		System.out.println("Ho attivato il turbo!!!");
+	}
+	
+	@Override
+	public String toString() {
+		
+		return super.toString()
+				+ "\ndoors count: " + DOORS_COUNT;
+	}
 }
--- Workspace/2023-09-20-java-abs-1/src/org/java/cars/UtilityCar.java
+++ Workspace/2023-09-20-java-abs-1/src/org/java/cars/UtilityCar.java
@@ -1,5 +1,34 @@
 package org.java.cars;
 
-public class UtilityCar {
+import org.java.cars.abs.Auto;
 
+public class UtilityCar extends Auto {
+
+	private int doorsCount;
+	
+	public UtilityCar(String name, int maxSpeed, int doorCount) {
+		super(name, maxSpeed);
+		
+		setDoorsCount(doorCount);
+	}
+
+	@Override
+	public void stampaNumeroPorte() {
+		
+		System.out.println("Doors count: " + getDoorsCount());
+	}
+	
+	public int getDoorsCount() {
+		return doorsCount;
+	}
+	public void setDoorsCount(int doorsCount) {
+		this.doorsCount = doorsCount;
+	}
+	
+	@Override
+	public String toString() {
+		
+		return super.toString() 
+				+ "doors count: " + getDoorsCount();
+	}
 }
