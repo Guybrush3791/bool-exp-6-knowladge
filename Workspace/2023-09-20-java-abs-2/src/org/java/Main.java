c67d679f2dffad9e53899792a1459bc2e9864bbd
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-20 12:42:32 +0200

    vault backup: 2023-09-20 12:42:32

--- Workspace/2023-09-20-java-abs-2/src/org/java/Main.java
+++ Workspace/2023-09-20-java-abs-2/src/org/java/Main.java
@@ -1,5 +1,44 @@
 package org.java;
 
+import org.java.shapes.Rectangle;
+import org.java.shapes.Square;
+import org.java.shapes.inter.PolygonInterface;
+
 public class Main {
 
+	public static void main(String[] args) {
+		
+		Square s1 = new Square(5);
+		System.out.println(s1);
+		
+		System.out.println("\n-----------------------\n");
+		
+		Rectangle r1 = new Rectangle(3, 5);
+		System.out.println(r1);
+		
+		System.out.println("\n-----------------------\n");
+		
+		PolygonInterface[] shapes = new PolygonInterface[2];
+		shapes[0] = s1;
+		shapes[1] = r1;
+		
+		int avgPer = 0;
+		int avgArea = 0;
+		for (int x=0;x<shapes.length;x++) {
+			
+			PolygonInterface shape = shapes[x];
+			
+			avgPer += shape.calcolaPerimetro();
+			avgArea += shape.calcolaArea();
+						
+			System.out.println(shape);
+			System.out.println("\n-----------------------\n");
+		}
+		
+		avgPer /= shapes.length;
+		avgArea /= shapes.length;
+		
+		System.out.println("media perimetro: " + avgPer);
+		System.out.println("media area: " + avgArea);
+	}
 }
