13174de8f5f02905064fd197a827e1f470c310ed
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-20 11:47:32 +0200

    vault backup: 2023-09-20 11:47:32

--- Workspace/2023-09-20-java-abs-1/src/org/java/cars/Auto.java
+++ Workspace/2023-09-20-java-abs-1/src/org/java/cars/Auto.java
@@ -1,5 +1,60 @@
 package org.java.cars;
 
-public class Auto {
+public abstract class Auto {
 
+	private String name;
+	private int maxSpeed;
+	private int doorsCount;
+	
+	public Auto(String name, int maxSpeed, int doorsCount) {
+		
+		setName(name);
+		setMaxSpeed(maxSpeed);
+		setDoorsCount(doorsCount);
+	}
+
+	public void stampaVelocitaMassima() {
+		
+		System.out.println("max speed: " + getMaxSpeed());
+	}
+	public void stampaNumeroPorte() {
+		
+		System.out.println("doors count: " + getDoorsCount());
+	}
+	public void accenditi() {
+		
+		System.out.println("turning on");
+	}
+	public void spegniti() {
+		
+		System.out.println("turning off");
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getMaxSpeed() {
+		return maxSpeed;
+	}
+	public void setMaxSpeed(int maxSpeed) {
+		this.maxSpeed = maxSpeed;
+	}
+	public int getDoorsCount() {
+		return doorsCount;
+	}
+	public void setDoorsCount(int doorsCount) {
+		this.doorsCount = doorsCount;
+	}
+	
+	@Override
+	public String toString() {
+		
+		return getName()  
+				+ ":\nmax speed: " + getMaxSpeed() 
+				+ "\ndoors count: " + getDoorsCount()
+		;
+	}
 }
