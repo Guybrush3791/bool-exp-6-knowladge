07c3327efceb4371d6bc7a48e24a100aa701a207
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-20 09:37:32 +0200

    vault backup: 2023-09-20 09:37:32

--- Workspace/2023-09-20-java-exceptions-books/src/org/java/Main.java
+++ Workspace/2023-09-20-java-exceptions-books/src/org/java/Main.java
@@ -1,11 +1,13 @@
 package org.java;
 
+import java.io.FileWriter;
 import java.util.Scanner;
 
 import org.java.library.Libro;
 
 public class Main {
 
+	private static final String LOG_PATH = "/tmp/tmp/java-log.out";
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
@@ -30,16 +32,26 @@ public class Main {
 			System.out.print("Editore: ");
 			String editore = sc.nextLine();
 			
-			Libro l = new Libro(titolo, nPagine, autore, editore);
-			libri[x] = l;
+			try {
+				
+				Libro l = new Libro(titolo, nPagine, autore, editore);
+				libri[x] = l;
+			} catch (Exception e) {
+				
+				System.out.println("Il libro non puo' essere creato: " + e.getMessage());
+				x--;
+			}
 			
 			System.out.println("\n--------------------------------------\n");
 		}
 		
+		FileWriter writer = new FileWriter(null)
 		for (int x=0;x<nLibri;x++) {
 			
 			Libro l = libri[x];
 			System.out.println("Libro " + (x + 1) + ":\n" + l);
+			
+			
 		}
 	}
 }
