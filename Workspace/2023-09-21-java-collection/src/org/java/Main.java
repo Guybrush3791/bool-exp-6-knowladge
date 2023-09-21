c15624354ee305c8ce5249e698d5bda687e2056e
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-21 10:38:01 +0200

    vault backup: 2023-09-21 10:38:01

--- Workspace/2023-09-21-java-collection/src/org/java/Main.java
+++ Workspace/2023-09-21-java-collection/src/org/java/Main.java
@@ -12,22 +12,22 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		List<String> strings = new ArrayList<>();
-		
-		strings.add("Hello");
-		strings.add("World!");
-		
-		System.out.println(strings);
-		
-		Random rnd = new Random();
-		List<Integer> values = new ArrayList<>();
-		for (int x=0;x<100;x++) {
-			
-			int value = rnd.nextInt(0, 10);
-			values.add(value);
-		}
-		
-		System.out.println(values);
+//		List<String> strings = new ArrayList<>();
+//		
+//		strings.add("Hello");
+//		strings.add("World!");
+//		
+//		System.out.println(strings);
+//		
+//		Random rnd = new Random();
+//		List<Integer> values = new ArrayList<>();
+//		for (int x=0;x<100;x++) {
+//			
+//			int value = rnd.nextInt(0, 10);
+//			values.add(value);
+//		}
+//		
+//		System.out.println(values);
 		
 		Set<Integer> uniqueValues = new HashSet<>();
 		for (int x=0;x<100;x++) {
@@ -38,21 +38,21 @@ public class Main {
 		
 		System.out.println(uniqueValues);
 		
-		String lorem = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti pariatur temporibus molestias nobis saepe maxime libero nulla neque officia iste quidem, reprehenderit beatae quam! Odio labore libero aut itaque voluptate?";
-		Map<Character, Integer> charCounter = new HashMap<>();
-		for (int x=0;x<lorem.length();x++) {
-			
-			char loremChar = lorem.toLowerCase().charAt(x);
-			
-			if (charCounter.containsKey(loremChar)) {
-				
-				charCounter.put(loremChar, charCounter.get(loremChar) + 1);
-			} else {
-				
-				charCounter.put(loremChar, 1);
-			}
-		}
-		
-		System.out.println(charCounter);
+//		String lorem = "aabbccddefg";
+//		Map<Character, Integer> charCounter = new HashMap<>();
+//		for (int x=0;x<lorem.length();x++) {
+//			
+//			char loremChar = lorem.toLowerCase().charAt(x);
+//			
+//			if (charCounter.containsKey(loremChar)) {
+//				
+//				charCounter.put(loremChar, charCounter.get(loremChar) + 1);
+//			} else {
+//				
+//				charCounter.put(loremChar, 1);
+//			}
+//		}
+//		
+//		System.out.println(charCounter);
 	}
 }
