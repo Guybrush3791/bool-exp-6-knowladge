76cdd69b6a68babb3c7290bb4b9e1c0b704e1b95
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-21 11:03:01 +0200

    vault backup: 2023-09-21 11:03:01

--- Workspace/2023-09-21-java-collection/src/org/java/Main.java
+++ Workspace/2023-09-21-java-collection/src/org/java/Main.java
@@ -1,7 +1,8 @@
 package org.java;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class Main {
@@ -10,21 +11,21 @@ public class Main {
 		
 		Random rnd = new Random();
 		
-		List<String> strings = new ArrayList<>();
-		
-		strings.add("Hello");
-		strings.add("World!");
-		
-		System.out.println(strings);
-		
-		List<Integer> values = new ArrayList<>();
-		for (int x=0;x<100;x++) {
-			
-			int value = rnd.nextInt(0, 10);
-			values.add(value);
-		}
-		
-		System.out.println(values);
+//		List<String> strings = new ArrayList<>();
+//		
+//		strings.add("Hello");
+//		strings.add("World!");
+//		
+//		System.out.println(strings);
+//		
+//		List<Integer> values = new ArrayList<>();
+//		for (int x=0;x<100;x++) {
+//			
+//			int value = rnd.nextInt(0, 10);
+//			values.add(value);
+//		}
+//		
+//		System.out.println(values);
 //		
 //		Set<Integer> uniqueValues = new HashSet<>();
 //		for (int x=0;x<100;x++) {
@@ -51,31 +52,33 @@ public class Main {
 //			System.out.println("value: " + value);
 //		}
 //		
-//		String lorem = "aabbccddefg";
-//		Map<Character, Integer> charCounter = new HashMap<>();
-//		for (int x=0;x<lorem.length();x++) {
-//			
-//			char loremChar = lorem.toLowerCase().charAt(x);
-//			
-//			if (charCounter.containsKey(loremChar)) {
-//				
-//				charCounter.put(loremChar, charCounter.get(loremChar) + 1);
-//			} else {
-//				
-//				charCounter.put(loremChar, 1);
-//			}
-//		}
-//		
-//		for (Character k : charCounter.keySet()) {
-//			
-//			Integer v = charCounter.get(k);
-//			
-//			System.out.println(k + " --> " + v);
-//		}
-//		
-//		Collection<Integer> values = charCounter.values();
-//		for (Integer value : values) { 
-//			System.out.println("value: " + value);
-//		}
+		String lorem = "aabbccddefg";
+		Map<Character, Integer> charCounter = new HashMap<>();
+		for (int x=0;x<lorem.length();x++) {
+			
+			char loremChar = lorem.toLowerCase().charAt(x);
+			
+			if (charCounter.containsKey(loremChar)) {
+				
+				charCounter.put(loremChar, charCounter.get(loremChar) + 1);
+			} else {
+				
+				charCounter.put(loremChar, 1);
+			}
+		}
+		
+		for (Character k : charCounter.keySet()) {
+			
+			Integer v = charCounter.get(k);
+			
+			System.out.println(k + " --> " + v);
+		}
+		
+		Collection<Integer> values = charCounter.values();
+		for (Integer value : values) { 
+			System.out.println("value: " + value);
+		}
+		
+//		charCounter.
 	}
 }
