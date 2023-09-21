dc829ca8e5bd1fbc2645b53bc6ce4b8092b60179
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-21 10:28:01 +0200

    vault backup: 2023-09-21 10:28:01

--- Workspace/2023-09-21-java-collection/src/org/java/Main.java
+++ Workspace/2023-09-21-java-collection/src/org/java/Main.java
@@ -1,8 +1,10 @@
 package org.java;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
 
 public class Main {
 
@@ -24,5 +26,14 @@ public class Main {
 		}
 		
 		System.out.println(values);
+		
+		Set<Integer> uniqueValues = new HashSet<>();
+		for (int x=0;x<100;x++) {
+			
+			int value = rnd.nextInt(0, 10);
+			uniqueValues.add(value);
+		}
+		
+		System.out.println(uniqueValues);
 	}
 }
