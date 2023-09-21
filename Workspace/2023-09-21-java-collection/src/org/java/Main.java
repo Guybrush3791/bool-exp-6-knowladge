9de902a84cd0099ce3e0b37fc150c962cef3ae2c
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-21 10:23:01 +0200

    vault backup: 2023-09-21 10:23:01

--- Workspace/2023-09-21-java-collection/src/org/java/Main.java
+++ Workspace/2023-09-21-java-collection/src/org/java/Main.java
@@ -1,5 +1,28 @@
 package org.java;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
 
+	public static void main(String[] args) {
+		
+		List<String> strings = new ArrayList<>();
+		
+		strings.add("Hello");
+		strings.add("World!");
+		
+		System.out.println(strings);
+		
+		Random rnd = new Random();
+		List<Integer> values = new ArrayList<>();
+		for (int x=0;x<100;x++) {
+			
+			int value = rnd.nextInt(0, 10);
+			values.add(value);
+		}
+		
+		System.out.println(values);
+	}
 }
