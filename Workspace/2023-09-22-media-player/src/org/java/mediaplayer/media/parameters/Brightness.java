8b11cd4d0af668f99807503ac3eaf0dbba941258
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-22 12:10:02 +0200

    vault backup: 2023-09-22 12:10:02

--- Workspace/2023-09-22-media-player/src/org/java/mediaplayer/media/parameters/Brightness.java
+++ Workspace/2023-09-22-media-player/src/org/java/mediaplayer/media/parameters/Brightness.java
@@ -2,8 +2,15 @@ package org.java.mediaplayer.media.parameters;
 
 public class Brightness {
 
+	private static final double MIN_BRIGHTNESS = 0d;
+	private static final double MAX_BRIGHTNESS = 1d;
+	
 	private double brightness;
 	
+	public Brightness() throws Exception {
+		
+		this(.5);
+	}
 	public Brightness(double brightness) throws Exception {
 		
 		setBrightness(brightness);
@@ -18,6 +25,16 @@ public class Brightness {
 			throw new Exception("Brightness must be from 0.0 to 1.0 included");
 		
 		this.brightness = brightness;
+	}
+	
+	
+	public void darker() {
+		
+		brightness -= brightness > MIN_BRIGHTNESS ? .1 : 0;
+	}
+	public void brighter() {
+		
+		brightness += brightness < MAX_BRIGHTNESS ? .1 : 0;
 	}
 	
 	@Override
--- Workspace/2023-09-22-media-player/src/org/java/mediaplayer/media/parameters/Volume.java
+++ Workspace/2023-09-22-media-player/src/org/java/mediaplayer/media/parameters/Volume.java
@@ -2,8 +2,15 @@ package org.java.mediaplayer.media.parameters;
 
 public class Volume {
 
+	private static final int MIN_VOLUME = 0;
+	private static final int MAX_VOLUME = 10;
+	
 	int volume;
 	
+	public Volume() throws Exception {
+		
+		this(5);
+	}
 	public Volume(int volume) throws Exception {
 		
 		setVolume(volume);
@@ -18,6 +25,15 @@ public class Volume {
 			throw new Exception("Volume must be from 0 to 10 included");
 			
 		this.volume = volume;
+	}
+	
+	public void weaker() {
+		
+		volume -= volume > MIN_VOLUME ? 1 : 0;
+	}
+	public void louder() {
+		
+		volume += volume < MAX_VOLUME ? 1 : 0;
 	}
 	
 	@Override
