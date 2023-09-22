704bbb8c167ecbbbb5cb08999746fbb33ccfd43e
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-22 14:41:34 +0200

    vault backup: 2023-09-22 14:41:34

--- Workspace/2023-09-22-media-player/src/org/java/mediaplayer/Main.java
+++ Workspace/2023-09-22-media-player/src/org/java/mediaplayer/Main.java
@@ -1,22 +1,54 @@
 package org.java.mediaplayer;
 
-import org.java.mediaplayer.media.Audio;
+import java.util.Scanner;
+
 import org.java.mediaplayer.media.Image;
-import org.java.mediaplayer.media.Video;
 import org.java.mediaplayer.media.abs.Media;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		try {
+		Scanner sc = new Scanner(System.in);
 		
-			Image i1 = new Image("mio immagine");
-			i1.execute();
-		} catch(Exception e) {
+		Media[] medias = new Media[5];
+		
+		for (int x=0;x<medias.length;x++) {
 			
-			System.out.println("Error: " + e.getMessage());
+			System.out.println("Media type:");
+			System.out.println("1) Image");
+			System.out.println("2) Audio");
+			System.out.println("3) Video");
+			System.out.println("------------");
+			String mediaType = sc.nextLine();
+			
+			System.out.print("\nTitolo: ");
+			String title = sc.nextLine();
+			
+			Media newMedia = null;
+			
+			try {
+				
+				switch(mediaType) {
+				
+					case "1":
+						newMedia = new Image(title);
+					break;
+					case "2":
+						newMedia = new Audio(title);
+					break;
+					case "3":
+						newMedia = new Video(title);
+					break;
+					
+				}
+			} catch(Exception e) {
+				
+				System.out.println("Error: " + e.getMessage());
+			}
 		}
+		
+		
 		
 		System.out.println("-----------------------------------");
 		System.out.println("The End");
