18deabe8e26da7bf2c0a2bad87b3658de3ba5fa8
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-22 14:46:34 +0200

    vault backup: 2023-09-22 14:46:34

--- Workspace/2023-09-22-media-player/src/org/java/mediaplayer/Main.java
+++ Workspace/2023-09-22-media-player/src/org/java/mediaplayer/Main.java
@@ -1,19 +1,24 @@
 package org.java.mediaplayer;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
+import org.java.mediaplayer.media.Audio;
 import org.java.mediaplayer.media.Image;
+import org.java.mediaplayer.media.Video;
 import org.java.mediaplayer.media.abs.Media;
 
 public class Main {
 
+	private static final int MEDIA_COUNT = 2;
+	
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		Media[] medias = new Media[5];
+		Media[] medias = new Media[MEDIA_COUNT];
 		
-		for (int x=0;x<medias.length;x++) {
+		for (int x=0;x<MEDIA_COUNT;x++) {
 			
 			System.out.println("Media type:");
 			System.out.println("1) Image");
@@ -39,16 +44,31 @@ public class Main {
 					break;
 					case "3":
 						newMedia = new Video(title);
-					break;
-					
+					break;					
 				}
 			} catch(Exception e) {
 				
 				System.out.println("Error: " + e.getMessage());
 			}
+			
+			if (newMedia == null) 
+				x--;
+			else 
+				medias[x] = newMedia;
 		}
 		
+		System.out.println("-----------------------------------");
 		
+		String userValue = "-1";
+		while (!userValue.equals("0")) {
+			
+			System.out.println("Media:");
+			for (int x=0;x<MEDIA_COUNT;x++) 
+				System.out.println(medias[x]
+						+ "-----------------------------------\n");
+			
+			userValue = sc.nextLine();
+		}
 		
 		System.out.println("-----------------------------------");
 		System.out.println("The End");
