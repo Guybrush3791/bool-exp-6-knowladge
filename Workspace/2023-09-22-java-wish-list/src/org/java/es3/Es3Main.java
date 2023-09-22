a341fe5e11d839feeddf93645b049ae5d5462d6c
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-22 10:15:02 +0200

    vault backup: 2023-09-22 10:15:02

--- Workspace/2023-09-22-java-wish-list/src/org/java/es3/Es3Main.java
+++ Workspace/2023-09-22-java-wish-list/src/org/java/es3/Es3Main.java
@@ -1,18 +1,41 @@
 package org.java.es3;
 
-import java.util.Random;
+import java.util.Scanner;
 
 public class Es3Main {
 	
 	public static void main(String[] args) {
 	
-		Random rnd = new Random(100);
+//		Random rnd = new Random(100);
+//		
+//		int[] values = new int[10];
+//		for (int x=0;x<10;x++) 
+//			values[x] = rnd.nextInt(0, 101);
+//		
+//		MyIterator myIt = new MyIterator(values);
+//		
+//		while(myIt.hasNext()) {
+//			
+//			int value = myIt.getNextElement();
+//			
+//			System.out.println("value: " + value);
+//		}
 		
-		int[] values = new int[10];
-		for (int x=0;x<10;x++) 
-			values[x] = rnd.nextInt(0, 101);
+		Scanner sc = new Scanner(System.in);
 		
-		MyIterator myIt = new MyIterator(values);
+		MyIterator myIt = new MyIterator();
+		
+		String userContinue = "y";
+		while (userContinue.equalsIgnoreCase("y")) {
+			
+			System.out.print("Valore: ");
+			int value = Integer.valueOf(sc.nextLine());
+			
+			myIt.addElement(value);
+			
+			System.out.println("Altro elemento? [Y/n]");
+			userContinue = sc.nextLine();
+		}
 		
 		while(myIt.hasNext()) {
 			
--- Workspace/2023-09-22-java-wish-list/src/org/java/es3/MyIterator.java
+++ Workspace/2023-09-22-java-wish-list/src/org/java/es3/MyIterator.java
@@ -5,11 +5,31 @@ import java.util.Arrays;
 public class MyIterator {
 
 	int index = 0;
+	int counter = 0;
 	int[] values;
 	
-	public MyIterator(int[] values) {
+	public MyIterator() {
 		
-		setValues(values);
+		values = new int[16];
+	}
+//	public MyIterator(int[] values) {
+//		
+//		setValues(values);
+//	}
+	
+	public void addElement(int value) {
+		
+		values[counter++] = value;
+		
+		if (counter == values.length) {
+			
+			int[] newValues = new int[values.length * 2];
+			for (int x=0;x<values.length;x++) {
+				newValues[x] = values[x];
+			}
+					
+			values = newValues;
+		}
 	}
 
 	public int getNextElement() {
