2005447f1535f2c66dcba85e011fa346efce21ca
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-26 09:59:33 +0200

    vault backup: 2023-09-26 09:59:33

--- Workspace/2023-09-26-java-gestore-eventi/src/org/java/Main.java
+++ Workspace/2023-09-26-java-gestore-eventi/src/org/java/Main.java
@@ -1,38 +1,62 @@
 package org.java;
 
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = null;
+		
 		try {
-//			Evento e1 = new Evento("Mio evento", LocalDate.of(2023, 9, 29), 100);
-			Evento e1 = new Evento("Mio evento", "2023-09-29", 100);
+			sc = new Scanner(System.in);
+			
+			System.out.println("Nuovo evento:");
+			
+			System.out.print("titolo: ");
+			String title = sc.nextLine();
+			
+			System.out.print("date: ");
+			String date = sc.nextLine();
+			
+			System.out.print("numero posti totali: ");
+			int numeroPosti = Integer.valueOf(sc.nextLine());
+			
+			Evento e1 = new Evento(title, date, numeroPosti);
+			
 			System.out.println(e1);
 			System.out.println("\n----------------------------------------------\n");
 			
-			System.out.println("Posti totali: " + e1.getPostiTotali());
-			System.out.println("Posti prenotati: " + e1.getPostiPrenotati());
-			System.out.println("\n----------------------------------------------\n");
-			
-			e1.prenota();
-			
-			System.out.println("Posti totali: " + e1.getPostiTotali());
-			System.out.println("Posti prenotati: " + e1.getPostiPrenotati());
-			System.out.println("\n----------------------------------------------\n");
-			
-			e1.disdici();
-			
-			System.out.println("Posti totali: " + e1.getPostiTotali());
-			System.out.println("Posti prenotati: " + e1.getPostiPrenotati());
-			System.out.println("\n----------------------------------------------\n");
-			
-			e1.disdici();
+//			Evento e1 = new Evento("Mio evento", "2023-09-29", 100);
+//			System.out.println(e1);
+//			System.out.println("\n----------------------------------------------\n");
+//			
+//			System.out.println("Posti totali: " + e1.getPostiTotali());
+//			System.out.println("Posti prenotati: " + e1.getPostiPrenotati());
+//			System.out.println("\n----------------------------------------------\n");
+//			
+//			e1.prenota();
+//			
+//			System.out.println("Posti totali: " + e1.getPostiTotali());
+//			System.out.println("Posti prenotati: " + e1.getPostiPrenotati());
+//			System.out.println("\n----------------------------------------------\n");
+//			
+//			e1.disdici();
+//			
+//			System.out.println("Posti totali: " + e1.getPostiTotali());
+//			System.out.println("Posti prenotati: " + e1.getPostiPrenotati());
+//			System.out.println("\n----------------------------------------------\n");
+//			
+//			e1.disdici();
 			
 		} catch (Exception e) {
 			
 			System.out.println(e.getMessage());
+		} finally {
+			
+			if (sc != null) 
+				sc.close();
 		}
 	}
 }
