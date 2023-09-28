499876f4b153d004081cc55918dbea64f33b7b57
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-28 12:42:32 +0200

    vault backup: 2023-09-28 12:42:32

--- Workspace/2023-09-28-java-jdbc-maven/src/main/java/org/java/myjdbc/Main.java
+++ Workspace/2023-09-28-java-jdbc-maven/src/main/java/org/java/myjdbc/Main.java
@@ -1,9 +1,21 @@
 package org.java.myjdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("hello, world!");
+		String url = "jdbc:mysql://localhost:3306/db-aeroporto";
+		String user = "root";
+		String password = "code";
+		
+		try (Connection con = DriverManager.getConnection(url, user, password)) {
+			  
+			System.out.println("Connection to db...OK!");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
