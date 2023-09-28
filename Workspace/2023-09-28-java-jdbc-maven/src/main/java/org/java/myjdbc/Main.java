31909ffd89b409fbc13a9f365551f1fd3306dd78
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-28 12:47:32 +0200

    vault backup: 2023-09-28 12:47:32

--- Workspace/2023-09-28-java-jdbc-maven/src/main/java/org/java/myjdbc/Main.java
+++ Workspace/2023-09-28-java-jdbc-maven/src/main/java/org/java/myjdbc/Main.java
@@ -2,18 +2,38 @@ package org.java.myjdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		String url = "jdbc:mysql://localhost:3306/db-aeroporto";
-		String user = "root";
-		String password = "code";
+		final String url = "jdbc:mysql://localhost:3306/db-aeroporto";
+		final String user = "root";
+		final String password = "code";
+		
+		final String sql = " SELECT id, name, lastname, date_of_birth "
+						 + " FROM passengers p ";	
 		
 		try (Connection con = DriverManager.getConnection(url, user, password)) {
 			  
-			System.out.println("Connection to db...OK!");
+			PreparedStatement ps = con.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				
+				int id = rs.getInt("id");
+				String name = rs.getString("name");
+				String lastname = rs.getString("lastname");
+				String dateOfBirth = rs.getString("date_of_birth");
+				
+				System.out.println("Passegero\n");
+				System.out.println("name: " + name + " " + lastname);
+				System.out.println("date of birth: " + dateOfBirth);
+				
+				System.out.println("\n------------------------------\n");
+			}
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
