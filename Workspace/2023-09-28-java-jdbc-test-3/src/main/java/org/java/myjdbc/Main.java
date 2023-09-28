5d75f5c155ee2b19ec9fd6a76d35542c0babfae2
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-28 14:49:54 +0200

    vault backup: 2023-09-28 14:49:54

--- Workspace/2023-09-28-java-jdbc-test-3/src/main/java/org/java/myjdbc/Main.java
+++ Workspace/2023-09-28-java-jdbc-test-3/src/main/java/org/java/myjdbc/Main.java
@@ -1,11 +1,26 @@
 package org.java.myjdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		final String url = "jdbc:mysql://localhost:$PORT/$DB_NAME";
+//		final String url = "jdbc:mysql://localhost:$PORT/$DB_NAME";
+		final String url = "jdbc:mysql://localhost:3306/db-aeroporto";
 		final String user = "root";
 		final String password = "code";
+		
+		try (Connection conn = DriverManager.getConnection(url, user, password)) {
+			
+			System.out.println("Connessione stabilita correttamente");
+		} catch (Exception e) {
+			
+			System.out.println("Errore di connessione: " + e.getMessage());
+		}
+		
+		System.out.println("\n----------------------------------\n");
+		System.out.println("The end");
 	}
 }
