e4d44b893dc69ce18ac39daed1826ae4f113cdaf
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-28 12:27:32 +0200

    vault backup: 2023-09-28 12:27:32

---
+++ Workspace/2023-09-28-java-jdbc/src/Main.java
@@ -0,0 +1,13 @@
+import java.sql.Connection;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String url = "jdbc:mysql://localhost:3306/test";
+		String user = "root";
+		String password = "rootpassword";
+		 
+		Connection con;
+	}
+}
