ac35603d6be21561bed11aa52930b7c37d81fdec
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-29 09:34:13 +0200

    vault backup: 2023-09-29 09:34:13

---
+++ Workspace/2023-09-29-db-nations/src/main/java/org/java/Main.java
@@ -0,0 +1,25 @@
+package org.java;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		final String url = "jdbc:mysql://localhost:3306/db_nations";
+		final String user = "root";
+		final String password = "code";
+		
+		try (Connection conn = DriverManager.getConnection(url, user, password)) {
+			
+			System.out.println("Connessione stabilita correttamente");
+		} catch (Exception e) {
+			
+			System.out.println("Errore di connessione: " + e.getMessage());
+		}
+		
+		System.out.println("\n----------------------------------\n");
+		System.out.println("The end");
+	}
+}
