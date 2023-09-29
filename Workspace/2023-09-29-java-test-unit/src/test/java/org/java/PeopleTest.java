551bfc5edb41d399420ca1e342fffec98bea1405
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-29 11:19:14 +0200

    vault backup: 2023-09-29 11:19:13

--- Workspace/2023-09-29-java-test-unit/src/test/java/org/java/PeopleTest.java
+++ Workspace/2023-09-29-java-test-unit/src/test/java/org/java/PeopleTest.java
@@ -1,6 +1,10 @@
 package org.java;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PeopleTest {
@@ -14,9 +18,15 @@ public class PeopleTest {
 	}
 	
 	@Test
+	@DisplayName("Test add person to people")	
 	public void addPersonToPeople() {
 		
+//		int beforeSize = people.getSize();
 		
 		people.addPerson(50, "Tulio");
+		
+		int afterSize = people.getSize();
+		
+		assertEquals(5, afterSize);
 	}
 }
