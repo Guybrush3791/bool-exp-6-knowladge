b7c0f09d2da7b92bed6badedcd1c07cc2b0facbe
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-09-29 09:39:13 +0200

    vault backup: 2023-09-29 09:39:13

--- Workspace/2023-09-29-db-nations/src/main/java/org/java/Main.java
+++ Workspace/2023-09-29-db-nations/src/main/java/org/java/Main.java
@@ -2,6 +2,8 @@ package org.java;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class Main {
 
@@ -13,7 +15,30 @@ public class Main {
 		
 		try (Connection conn = DriverManager.getConnection(url, user, password)) {
 			
-			System.out.println("Connessione stabilita correttamente");
+			final String sql = " SELECT c.country_id, c.name 'nation', r.name 'country', c2.name 'continent'  "
+							 + " FROM countries c  "
+							 + "	JOIN regions r "
+							 + "		ON c.region_id = r.region_id"
+							 + "	JOIN continents c2 "
+							 + "		ON r.continent_id = c2.continent_id "
+							 + " ORDER BY c.name ";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				
+				int id = rs.getInt("country_id");
+				String nation = rs.getString("nation");
+				String country = rs.getString("country");
+				String continent = rs.getString("continent");
+				
+				System.out.println("[" + id + "] " + nation);
+				System.out.println("country: " + country);
+				System.out.println("continent: " + continent);
+				
+				System.out.println("\n---------------------------------\n");
+						
+			}
 		} catch (Exception e) {
 			
 			System.out.println("Errore di connessione: " + e.getMessage());
