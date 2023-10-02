00604d991f23fd60ccee72f7869800a11b158f8e
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-10-02 09:24:56 +0200

    vault backup: 2023-10-02 09:24:56

---
+++ Workspace/2023-10-02-java-calculator-tester/src/main/java/org/java/calc/Calculator.java
@@ -0,0 +1,21 @@
+package org.java.calc;
+
+public class Calculator {
+
+	public float add(float num1, float num2) {
+			
+			return num1 + num2;
+		}
+	public float subtracr(float num1, float num2) {
+		
+		return num1 - num2;
+	}
+	public float divide(float num1, float num2) {
+		
+		return num1 / num2;
+	}
+	public float multiply(float num1, float num2) {
+		
+		return num1 * num2;
+	}
+}
