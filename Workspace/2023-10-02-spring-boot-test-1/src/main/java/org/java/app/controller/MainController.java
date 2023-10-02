ed3ecfb4ef7062c7c489dfa3e362652c81ed9156
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-10-02 11:49:57 +0200

    vault backup: 2023-10-02 11:49:57

--- Workspace/2023-10-02-spring-boot-test-1/src/main/java/org/java/app/controller/MainController.java
+++ Workspace/2023-10-02-spring-boot-test-1/src/main/java/org/java/app/controller/MainController.java
@@ -1,10 +1,14 @@
 package org.java.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
+	@GetMapping("/")
+	@ResponseBody
 	public String test() {
 		
 		return "Hello, World!";
