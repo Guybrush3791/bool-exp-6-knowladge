8f672c72980a85e712a5483d63355e4af05d1bd6
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-10-02 11:44:57 +0200

    vault backup: 2023-10-02 11:44:56

---
+++ Workspace/2023-10-02-spring-boot-test-1/src/main/java/org/java/app/controller/MainController.java
@@ -0,0 +1,12 @@
+package org.java.app.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainController {
+
+	public String test() {
+		
+		return "Hello, World!";
+	}
+}
