042293310acc1ba993f591ee811dfc29d6076f81
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-10-03 09:51:04 +0200

    vault backup: 2023-10-03 09:51:04

--- Workspace/2023-10-03-best-of-the-year/src/main/java/org/java/app/controller/MainController.java
+++ Workspace/2023-10-03-best-of-the-year/src/main/java/org/java/app/controller/MainController.java
@@ -2,6 +2,7 @@ package org.java.app.controller;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.java.app.pojo.Movie;
 import org.java.app.pojo.Song;
@@ -24,16 +25,32 @@ public class MainController {
 	@GetMapping("/movies")
 	public String getMoviesIndex(Model model) {
 		
-		String strMovies = "";
-		for (Movie movie : getMovies()) {
-			
-			strMovies += movie.getTitle() + ", ";
-		}
-		strMovies = strMovies.substring(0, strMovies.length() - 2);
-		
+//		String strMovies = "";
+//		for (Movie movie : getMovies()) {
+//			
+//			strMovies += movie.getTitle() + ", ";
+//		}
+//		strMovies = strMovies.substring(0, strMovies.length() - 2);
+		String strMovies = getMovies().stream()
+//								.map(m -> m.getTitle())
+								.map(Movie::getTitle)
+							.collect(Collectors.joining(", "));
+				
 		model.addAttribute("strMovies", strMovies);
 		
 		return "movies-index";
+	}
+	
+	@GetMapping("/songs")
+	public String getSongsIndex(Model model) {
+		
+		String strSongs = getSongs().stream()
+								.map(Song::getTitle)
+							.collect(Collectors.joining(", "));
+		
+		model.addAttribute("strSongs", strSongs);
+		
+		return "songs-index";
 	}
 	
 	private List<Movie> getMovies() {
