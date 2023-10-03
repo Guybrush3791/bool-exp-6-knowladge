72e1a354e95654523c662b275d595f040cfe7b85
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-10-03 09:41:04 +0200

    vault backup: 2023-10-03 09:41:04

--- Workspace/2023-10-03-best-of-the-year/src/main/java/org/java/app/controller/MainController.java
+++ Workspace/2023-10-03-best-of-the-year/src/main/java/org/java/app/controller/MainController.java
@@ -1,5 +1,18 @@
 package org.java.app.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainController {
 
+	@GetMapping("/")
+	public String getIndex(Model model) {
+		
+		final String name = "Guybrush";
+		model.addAttribute("name", name);
+		
+		return "index";
+	}
 }
---
+++ Workspace/2023-10-03-best-of-the-year/src/main/java/org/java/app/pojo/Song.java
@@ -0,0 +1,32 @@
+package org.java.app.pojo;
+
+public class Song {
+
+	private int id;
+	private String title;
+	
+	public Song(int id, String title) {
+		
+		setId(id);
+		setTitle(title);
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	@Override
+	public String toString() {
+		
+		return "Song: [" + getId() + "] " + getTitle();
+	}
+}
