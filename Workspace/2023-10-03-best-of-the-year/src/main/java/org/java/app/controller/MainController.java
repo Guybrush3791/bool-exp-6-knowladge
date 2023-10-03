b1dddd7a3fe95f4b897062046c0a54b39f1cc877
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-10-03 09:46:04 +0200

    vault backup: 2023-10-03 09:46:04

--- Workspace/2023-10-03-best-of-the-year/src/main/java/org/java/app/controller/MainController.java
+++ Workspace/2023-10-03-best-of-the-year/src/main/java/org/java/app/controller/MainController.java
@@ -1,5 +1,10 @@
 package org.java.app.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.java.app.pojo.Movie;
+import org.java.app.pojo.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,5 +19,41 @@ public class MainController {
 		model.addAttribute("name", name);
 		
 		return "index";
+	}
+	
+	@GetMapping("/movies")
+	public String getMoviesIndex(Model model) {
+		
+		String strMovies = "";
+		for (Movie movie : getMovies()) {
+			
+			strMovies += movie.getTitle() + ", ";
+		}
+		strMovies = strMovies.substring(0, strMovies.length() - 2);
+		
+		model.addAttribute("strMovies", strMovies);
+		
+		return "movies-index";
+	}
+	
+	private List<Movie> getMovies() {
+		
+		return Arrays.asList(new Movie[] {
+				new Movie(1, "movie 1"),
+				new Movie(2, "movie 2"),
+				new Movie(3, "movie 3"),
+				new Movie(4, "movie 4"),
+				new Movie(5, "movie 5")
+		});
+	}
+	private List<Song> getSongs() {
+		
+		return Arrays.asList(new Song[] {
+				new Song(1, "song 1"),
+				new Song(2, "song 2"),
+				new Song(3, "song 3"),
+				new Song(4, "song 4"),
+				new Song(5, "song 5")
+		});
 	}
 }
