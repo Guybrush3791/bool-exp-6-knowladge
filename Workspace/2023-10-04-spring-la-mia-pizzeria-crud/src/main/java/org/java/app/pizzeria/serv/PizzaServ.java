450c6dba336135b6ff8d4950ebb506ac3275116d
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-10-05 09:34:17 +0200

    vault backup: 2023-10-05 09:34:17

---
+++ Workspace/2023-10-04-spring-la-mia-pizzeria-crud/src/main/java/org/java/app/pizzeria/serv/PizzaServ.java
@@ -0,0 +1,28 @@
+package org.java.app.pizzeria.serv;
+
+import java.util.List;
+
+import org.java.app.pizzeria.pojo.Pizza;
+import org.java.app.pizzeria.repo.PizzaRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PizzaServ {
+
+	@Autowired
+	private PizzaRepo pizzaRepo;
+	
+	public void save(Pizza pizza) {
+		
+		pizzaRepo.save(pizza);
+	}
+	public List<Pizza> findAll() {
+		
+		return pizzaRepo.findAll();
+	}
+	public Pizza findById(int id) {
+		
+		return pizzaRepo.findById(id).get();
+	}
+}
