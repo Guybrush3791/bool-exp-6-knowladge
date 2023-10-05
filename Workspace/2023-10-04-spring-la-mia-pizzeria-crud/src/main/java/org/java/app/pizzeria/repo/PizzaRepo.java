aa37984ef4316a0b170c036e0965dc7993d6cf5e
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-10-05 10:09:17 +0200

    vault backup: 2023-10-05 10:09:17

--- Workspace/2023-10-04-spring-la-mia-pizzeria-crud/src/main/java/org/java/app/pizzeria/repo/PizzaRepo.java
+++ Workspace/2023-10-04-spring-la-mia-pizzeria-crud/src/main/java/org/java/app/pizzeria/repo/PizzaRepo.java
@@ -1,5 +1,7 @@
 package org.java.app.pizzeria.repo;
 
+import java.util.List;
+
 import org.java.app.pizzeria.pojo.Pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
