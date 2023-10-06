cdcf771afdd399be0fcd18a7286ae677557a020a
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-10-06 10:14:15 +0200

    vault backup: 2023-10-06 10:14:15

--- Workspace/2023-10-04-spring-crud-1/src/main/java/org/java/app/mvc/controller/BookController.java
+++ Workspace/2023-10-04-spring-crud-1/src/main/java/org/java/app/mvc/controller/BookController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -50,5 +52,12 @@ public class BookController {
 		model.addAttribute("book", new Book());
 		
 		return "book-create";
+	}
+	@PostMapping("/create")
+	public String storeBook(@ModelAttribute Book book) {
+		
+		System.out.println("New book: " + book);
+		
+		return "redirect:/books";
 	}
 }
