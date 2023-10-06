d3816598188786481a2f4e988f10c22e9748b1bc
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-10-06 09:24:15 +0200

    vault backup: 2023-10-06 09:24:15

--- Workspace/2023-10-04-spring-la-mia-pizzeria-crud/src/main/java/org/java/app/Application.java
+++ Workspace/2023-10-04-spring-la-mia-pizzeria-crud/src/main/java/org/java/app/Application.java
@@ -20,16 +20,16 @@ public class Application implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-//		Pizza pizza1 = new Pizza("pizza 1", "desc pizza 1", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fit.wikipedia.org%2Fwiki%2FPizza_napoletana&psig=AOvVaw1XtZhspNkgHeHJuy1umRvU&ust=1696577642654000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCJD62K-y3oEDFQAAAAAdAAAAABAE", 10);
-//		Pizza pizza2 = new Pizza("pizza 2", "desc pizza 2", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fit.wikipedia.org%2Fwiki%2FPizza_napoletana&psig=AOvVaw1XtZhspNkgHeHJuy1umRvU&ust=1696577642654000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCJD62K-y3oEDFQAAAAAdAAAAABAE", 12);
-//		Pizza pizza3 = new Pizza("pizza 3", "desc pizza 3", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fit.wikipedia.org%2Fwiki%2FPizza_napoletana&psig=AOvVaw1XtZhspNkgHeHJuy1umRvU&ust=1696577642654000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCJD62K-y3oEDFQAAAAAdAAAAABAE", 14);
-//		Pizza pizza4 = new Pizza("pizza 4", "desc pizza 4", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fit.wikipedia.org%2Fwiki%2FPizza_napoletana&psig=AOvVaw1XtZhspNkgHeHJuy1umRvU&ust=1696577642654000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCJD62K-y3oEDFQAAAAAdAAAAABAE", 16);
-//		
-//		pizzaServ.save(pizza1);
-//		pizzaServ.save(pizza2);
-//		pizzaServ.save(pizza3);
-//		pizzaServ.save(pizza4);
-//		
-//		System.out.println("Insert OK");
+		Pizza pizza1 = new Pizza("pizza 1", "desc pizza 1", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fit.wikipedia.org%2Fwiki%2FPizza_napoletana&psig=AOvVaw1XtZhspNkgHeHJuy1umRvU&ust=1696577642654000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCJD62K-y3oEDFQAAAAAdAAAAABAE", 10);
+		Pizza pizza2 = new Pizza("pizza 2", "desc pizza 2", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fit.wikipedia.org%2Fwiki%2FPizza_napoletana&psig=AOvVaw1XtZhspNkgHeHJuy1umRvU&ust=1696577642654000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCJD62K-y3oEDFQAAAAAdAAAAABAE", 12);
+		Pizza pizza3 = new Pizza("pizza 3", "desc pizza 3", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fit.wikipedia.org%2Fwiki%2FPizza_napoletana&psig=AOvVaw1XtZhspNkgHeHJuy1umRvU&ust=1696577642654000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCJD62K-y3oEDFQAAAAAdAAAAABAE", 14);
+		Pizza pizza4 = new Pizza("pizza 4", "desc pizza 4", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fit.wikipedia.org%2Fwiki%2FPizza_napoletana&psig=AOvVaw1XtZhspNkgHeHJuy1umRvU&ust=1696577642654000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCJD62K-y3oEDFQAAAAAdAAAAABAE", 16);
+		
+		pizzaServ.save(pizza1);
+		pizzaServ.save(pizza2);
+		pizzaServ.save(pizza3);
+		pizzaServ.save(pizza4);
+		
+		System.out.println("Insert OK");
 	}
 }
