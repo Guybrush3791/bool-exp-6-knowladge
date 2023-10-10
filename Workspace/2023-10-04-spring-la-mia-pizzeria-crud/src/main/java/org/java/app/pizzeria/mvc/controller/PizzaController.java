b3c7713e173b60df3611816a1872cd25111b8edd
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-10-10 09:33:49 +0200

    vault backup: 2023-10-10 09:33:49

--- Workspace/2023-10-04-spring-la-mia-pizzeria-crud/src/main/java/org/java/app/pizzeria/mvc/controller/PizzaController.java
+++ Workspace/2023-10-04-spring-la-mia-pizzeria-crud/src/main/java/org/java/app/pizzeria/mvc/controller/PizzaController.java
@@ -7,9 +7,14 @@ import org.java.app.pizzeria.serv.PizzaServ;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class PizzaController {
@@ -48,5 +53,16 @@ public class PizzaController {
 		model.addAttribute("pizza", new Pizza());
 		
 		return "pizza-create";
+	}
+	@PostMapping("/pizzas/create")
+	public String storeNewPizza(
+			@Valid @ModelAttribute Pizza pizza,
+			BindingResult br,
+			Model model
+		) {
+		
+		System.out.println("pizza:\n" + pizza);
+		
+		return "redirect:/pizzas/create";
 	}
 }
