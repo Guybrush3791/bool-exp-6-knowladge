d3ef9c075b4fcbfcc9c3447ad7ca342386bcb12e
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-10-11 10:04:44 +0200

    vault backup: 2023-10-11 10:04:44

--- Workspace/2023-10-04-spring-la-mia-pizzeria-crud/src/main/java/org/java/app/Application.java
+++ Workspace/2023-10-04-spring-la-mia-pizzeria-crud/src/main/java/org/java/app/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.mysql.cj.x.protobuf.MysqlxDatatypes.Scalar.String;
+
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
