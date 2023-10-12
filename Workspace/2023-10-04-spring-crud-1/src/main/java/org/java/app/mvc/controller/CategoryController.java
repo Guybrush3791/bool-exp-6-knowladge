0d49b37fbebe8a3a8ac7142dacec17ac1697a152
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-10-12 12:20:04 +0200

    vault backup: 2023-10-12 12:20:04

---
+++ Workspace/2023-10-04-spring-crud-1/src/main/java/org/java/app/mvc/controller/CategoryController.java
@@ -0,0 +1,34 @@
+package org.java.app.mvc.controller;
+
+import java.util.List;
+
+import org.java.app.db.pojo.Category;
+import org.java.app.db.serv.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/categories")
+public class CategoryController {
+
+	@Autowired
+	private CategoryService categoryService;
+	
+	@GetMapping
+	public String getIndex(Model model) {
+		
+		List<Category> categories = categoryService.findAll();
+		model.addAttribute("categories", categories);
+		
+		return "category-index";
+	}
+	
+	@GetMapping("/create")
+	public String getCreate() {
+		
+		return "category-create";
+	}
+}
