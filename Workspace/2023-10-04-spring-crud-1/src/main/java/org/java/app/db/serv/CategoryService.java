9ff6a7d84c62c52ed42e1977dc8e557e9ca60249
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-10-12 11:05:04 +0200

    vault backup: 2023-10-12 11:05:04

--- Workspace/2023-10-04-spring-crud-1/src/main/java/org/java/app/db/serv/CategoryService.java
+++ Workspace/2023-10-04-spring-crud-1/src/main/java/org/java/app/db/serv/CategoryService.java
@@ -1,5 +1,28 @@
 package org.java.app.db.serv;
 
+import java.util.List;
+
+import org.java.app.db.pojo.Category;
+import org.java.app.db.repo.CategoryRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CategoryService {
 
+	@Autowired
+	private CategoryRepo categoryRepo;
+	
+	public List<Category> findAll() {
+		
+		return categoryRepo.findAll();
+	}
+	public Category findById(int id) {
+		
+		return categoryRepo.findById(id).get();
+	}
+	public void save(Category category) {
+		
+		categoryRepo.save(category);
+	}
 }
