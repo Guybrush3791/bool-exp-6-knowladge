ae92e6bbcc6b5b9e5bc5a57ac89dba47e6fef02f
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-10-12 09:30:04 +0200

    vault backup: 2023-10-12 09:30:04

--- Workspace/2023-10-04-spring-la-mia-pizzeria-crud/src/main/java/org/java/app/pizzeria/pojo/SpecialOffert.java
+++ Workspace/2023-10-04-spring-la-mia-pizzeria-crud/src/main/java/org/java/app/pizzeria/pojo/SpecialOffert.java
@@ -1,5 +1,75 @@
 package org.java.app.pizzeria.pojo;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+
+@Entity
 public class SpecialOffert {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(nullable = false)
+	@Min(3)
+	@Max(255)
+	private String title;
+	
+	@Column(nullable = false)
+	private LocalDate startDate;
+	
+	@Column(nullable = false)
+	private LocalDate endDate;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Pizza pizza;
+	
+	public SpecialOffert() { }
+	public SpecialOffert(String title, LocalDate startDate, LocalDate endDate) {
+		
+		setTitle(title);
+		setStartDate(startDate);
+		setEndDate(endDate);
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
+	}
+	public LocalDate getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(LocalDate endDate) {
+		this.endDate = endDate;
+	}
+	
+	@Override
+	public String toString() {
+		
+		return "[" + getId() + "] " + getTitle() + ": " 
+				+ getStartDate() + " ~ " + getEndDate();
+	}
 }
