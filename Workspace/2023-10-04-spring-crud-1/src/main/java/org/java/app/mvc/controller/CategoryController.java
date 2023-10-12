7bccfeb18b18f68ece14b0d6e73fac97cb02b437
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-10-12 12:25:04 +0200

    vault backup: 2023-10-12 12:25:04

--- Workspace/2023-10-04-spring-crud-1/src/main/java/org/java/app/mvc/controller/CategoryController.java
+++ Workspace/2023-10-04-spring-crud-1/src/main/java/org/java/app/mvc/controller/CategoryController.java
@@ -2,7 +2,9 @@ package org.java.app.mvc.controller;
 
 import java.util.List;
 
+import org.java.app.db.pojo.Book;
 import org.java.app.db.pojo.Category;
+import org.java.app.db.serv.BookService;
 import org.java.app.db.serv.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,6 +16,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/categories")
 public class CategoryController {
 
+	@Autowired
+	private BookService bookService;
+	
 	@Autowired
 	private CategoryService categoryService;
 	
@@ -27,7 +32,13 @@ public class CategoryController {
 	}
 	
 	@GetMapping("/create")
-	public String getCreate() {
+	public String getCreate(Model model) {
+		
+		List<Book> books = bookService.findAll();
+		Category category = new Category();
+		
+		model.addAttribute("category", category);
+		model.addAttribute("books", books);
 		
 		return "category-create";
 	}
