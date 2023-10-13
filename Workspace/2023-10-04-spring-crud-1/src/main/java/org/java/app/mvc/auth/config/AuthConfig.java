b7d5167786ca3d03d11e53262af853f8a4505ceb
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-10-13 11:43:40 +0200

    vault backup: 2023-10-13 11:43:40

--- Workspace/2023-10-04-spring-crud-1/src/main/java/org/java/app/mvc/auth/config/AuthConfig.java
+++ Workspace/2023-10-04-spring-crud-1/src/main/java/org/java/app/mvc/auth/config/AuthConfig.java
@@ -1,8 +1,12 @@
 package org.java.app.mvc.auth.config;
 
+import org.java.app.mvc.auth.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,12 +17,33 @@ public class AuthConfig {
 		throws Exception {
 			 
 			http.authorizeHttpRequests()
-		        .requestMatchers("/user").hasAuthority("USER")
-		        .requestMatchers("/admin").hasAuthority("ADMIN")
-		        .requestMatchers("/").permitAll()
+		        .requestMatchers("/user/**").hasAuthority("USER")
+		        .requestMatchers("/admin/**").hasAuthority("ADMIN")
+		        .requestMatchers("/**").permitAll()
 		        .and().formLogin()
 		        .and().logout();
 			
 			return http.build();
-  }
+	}
+	
+	@Bean
+	UserService userDetailsService() {
+		return new UserService();
+	}
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+		
+		return new BCryptPasswordEncoder();
+	}
+    
+    @Bean
+    DaoAuthenticationProvider authenticationProvider() {
+      DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+   
+      authProvider.setUserDetailsService(userDetailsService());
+      authProvider.setPasswordEncoder(passwordEncoder());
+   
+      return authProvider;
+    }
 }
