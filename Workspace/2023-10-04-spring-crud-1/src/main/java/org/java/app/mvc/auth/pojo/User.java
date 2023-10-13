92b0d22ab1662de50205cf5aa860b850d70fb0ab
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-10-13 11:18:40 +0200

    vault backup: 2023-10-13 11:18:40

--- Workspace/2023-10-04-spring-crud-1/src/main/java/org/java/app/mvc/auth/pojo/User.java
+++ Workspace/2023-10-04-spring-crud-1/src/main/java/org/java/app/mvc/auth/pojo/User.java
@@ -1,6 +1,11 @@
 package org.java.app.mvc.auth.pojo;
 
+import java.util.Collection;
 import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -12,7 +17,7 @@ import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotNull;
 
 @Entity
-public class User {
+public class User implements UserDetails {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -54,10 +59,29 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	public Set<Role> getRoles() {
+		return roles;
+	}
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
 	
 	@Override
 	public String toString() {
 		
 		return "[" + getId() + "] " + getUsername();
 	}
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		
+		return getRoles().stream()
+				.map(r -> new SimpleGrantedAuthority(
+									r.getName()
+								)
+				).toList();
+	}
+	@Override public boolean isAccountNonExpired() { return true; }
+	@Override public boolean isAccountNonLocked() { return true; }
+	@Override public boolean isCredentialsNonExpired() { return true; }
+	@Override public boolean isEnabled() { return true; }
 }
