5dec07b59c2a47d971354eb6b418ff8b6f359816
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-10-13 11:33:40 +0200

    vault backup: 2023-10-13 11:33:40

---
+++ Workspace/2023-10-04-spring-crud-1/src/main/java/org/java/app/mvc/auth/config/AuthConfig.java
@@ -0,0 +1,23 @@
+package org.java.app.mvc.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class AuthConfig {
+
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http)
+		throws Exception {
+			return 
+				http.authorizeHttpRequests()
+			        .requestMatchers("/user").hasAuthority("USER")
+			        .requestMatchers("/admin").hasAuthority("ADMIN")
+			        .requestMatchers("/").permitAll()
+			        .and().formLogin()
+			        .and().logout()
+			        .build();
+  }
+}
