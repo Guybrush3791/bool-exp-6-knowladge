044a29cbbbae46c5ebfb31ffa8c6494589531c9a
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-10-16 10:58:39 +0200

    vault backup: 2023-10-16 10:58:39

--- Workspace/2023-10-04-spring-crud-1/src/main/java/org/java/app/api/BookRestController.java
+++ Workspace/2023-10-04-spring-crud-1/src/main/java/org/java/app/api/BookRestController.java
@@ -1,5 +1,6 @@
 package org.java.app.api;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.java.app.db.pojo.Book;
@@ -19,6 +20,52 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1.0")
 public class BookRestController {
 
+	public static class BookDTO {
+		
+		private int id;
+		private String title;
+		private LocalDate releaseDate;
+		private String isbn;
+		
+		public BookDTO() { }
+		public BookDTO(String title) {
+			
+			setTitle(title);
+		}
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public String getTitle() {
+			return title;
+		}
+		public void setTitle(String title) {
+			this.title = title;
+		}
+		public LocalDate getReleaseDate() {
+			return releaseDate;
+		}
+		public void setReleaseDate(LocalDate releaseDate) {
+			this.releaseDate = releaseDate;
+		}
+		public String getIsbn() {
+			return isbn;
+		}
+		public void setIsbn(String isbn) {
+			this.isbn = isbn;
+		}
+		
+		@Override
+		public String toString() {
+			
+			return "title: " + getTitle() 
+					+ "\nrelease date: " + getReleaseDate() 
+					+ "\nisbn: " + getIsbn();
+		}
+	}
+	
 	@Autowired
 	private BookService bookService;
 	
@@ -30,10 +77,14 @@ public class BookRestController {
 		return new ResponseEntity<>(books, HttpStatus.OK);
 	}
 	@PostMapping
-	public ResponseEntity<String> save() {
+	public ResponseEntity<String> save(
+			@RequestBody BookDTO bookDto
+		) {
 		
 //		System.out.println("Api book:\n" + book);
 //		System.out.println("Api value: " + value);
+		
+		System.out.println("Api book DTO:\n" + bookDto);
 		
 		return new ResponseEntity<>("OK", HttpStatus.OK);
 	}
