59b7034b3fa5f6b192aaf10f9fb354718748a12b
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-10-16 09:43:39 +0200

    vault backup: 2023-10-16 09:43:39

--- Workspace/2023-10-04-spring-la-mia-pizzeria-crud/src/main/java/org/java/app/pizzeria/auth/pojo/User.java
+++ Workspace/2023-10-04-spring-la-mia-pizzeria-crud/src/main/java/org/java/app/pizzeria/auth/pojo/User.java
@@ -1,6 +1,8 @@
 package org.java.app.pizzeria.auth.pojo;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
 
 import org.springframework.security.core.GrantedAuthority;
@@ -34,10 +36,11 @@ public class User implements UserDetails {
 	private Set<Role> roles;
 	
 	public User() { }
-	public User(String username, String password) {
+	public User(String username, String password, Role... roles) {
 		
 		setUsername(username);
 		setPassword(password);
+		setRoles(new HashSet<>(Arrays.asList(roles)));
 	}
 	
 	public int getId() {
